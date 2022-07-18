d5d3b2b747d2092e0407c73a94c9f28972ada652
Remo Griesbaum <remo.griesbaum@outlook.com>
2022-07-18 18:36:09 +0200

    feat(build): add Angular schematics

---
+++ projects/ngx-cytoscapejs/schematics/ng-add/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+// Just return the tree
+export function ngAdd(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    return tree;
+  };
+}
