8ee89286e7ef09c1afe7072562ca3b358cf266f7
Remo Griesbaum <remo.griesbaum@outlook.com>
2022-07-19 16:09:33 +0200

    feat(build): add @types/cytoscape when using ng-add

--- projects/ngx-cytoscapejs/schematics/ng-add/index.ts
+++ projects/ngx-cytoscapejs/schematics/ng-add/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable */
 import { workspaces } from '@angular-devkit/core';
 import {
   createSourceFile,
@@ -16,14 +16,15 @@ import { getWorkspace } from '@schematics/angular/utility/workspace';
 import { addImportToModule } from '@schematics/angular/utility/ast-utils';
 import { getAppModulePath } from '@schematics/angular/utility/ng-ast-utils';
 import { InsertChange } from '@schematics/angular/utility/change';
-// eslint-disable-next-line import/extensions
 import { Schema } from './schema';
 
 function getProjectTargetOptions(project: workspaces.ProjectDefinition, buildTarget: string) {
   const buildTargetObject = project.targets.get(buildTarget);
+
   if (buildTargetObject && buildTargetObject.options) {
     return buildTargetObject.options;
   }
+
   throw new SchematicsException(
     `Cannot determine project target configuration for: ${buildTarget}.`,
   );
@@ -31,18 +32,54 @@ function getProjectTargetOptions(project: workspaces.ProjectDefinition, buildTar
 
 function getSourceFile(tree: Tree, path: string) {
   const buffer = tree.read(path);
+
   if (!buffer) {
     throw new SchematicsException(`Could not find ${path}.`);
   }
+
   const content = buffer.toString('utf-8');
+
   return createSourceFile(path, content, ScriptTarget.Latest, true);
 }
 
-function addDependency(): Rule {
-  return (tree: Tree, context: SchematicContext) => {
-    context.addTask(new NodePackageInstallTask());
-    return tree;
-  };
+function getDevPackageVersionFromPackageJson(tree: Tree, name: string): string | null {
+  if (!tree.exists('package.json')) {
+    return null;
+  }
+
+  const packageJson = JSON.parse(tree.read('package.json')!.toString('utf8'));
+
+  if (packageJson.devDependencies && packageJson.devDependencies[name]) {
+    return packageJson.devDependencies[name];
+  }
+
+  return null;
+}
+
+function sortObjectByKeys(obj: Record<string, string>) {
+  return Object.keys(obj)
+    .sort()
+    .reduce((result: any, key) => (result[key] = obj[key]) && result, {});
+}
+
+function addDevPackageToPackageJson(tree: Tree, pkg: string, version: string): Tree {
+  if (tree.exists('package.json')) {
+    const sourceText = tree.read('package.json')!.toString('utf-8');
+    const json = JSON.parse(sourceText);
+
+    if (!json.devDependencies) {
+      json.devDependencies = {};
+    }
+
+    if (!json.devDependencies[pkg]) {
+      json.devDependencies[pkg] = version;
+      json.devDependencies = sortObjectByKeys(json.devDependencies);
+    }
+
+    tree.overwrite('package.json', JSON.stringify(json, null, 2));
+  }
+
+  return tree;
 }
 
 function addModule(options: Schema): Rule {
@@ -76,6 +113,25 @@ function addModule(options: Schema): Rule {
   };
 }
 
+function addCytoscapeTypes(): Rule {
+  return (tree: Tree) => {
+    const cytoscapeTypesVersion = getDevPackageVersionFromPackageJson(tree, '@types/cytoscape');
+
+    if (cytoscapeTypesVersion === null) {
+      addDevPackageToPackageJson(tree, '@types/cytoscape', '*');
+    }
+
+    return tree;
+  };
+}
+
+function addDependency(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    return tree;
+  };
+}
+
 export function ngAdd(options: Schema): Rule {
-  return chain([addDependency(), addModule(options)]);
+  return chain([addModule(options), addCytoscapeTypes(), addDependency()]);
 }
