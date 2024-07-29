85f8fe3bfb7afe729eeb9d53a80664d03ef9c563
Remo Griesbaum <remo.griesbaum@outlook.com>
2024-07-29 18:20:23 +0000

    feat: upgrade to Angular 18

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CytoscapejsModule } from 'ngx-cytoscapejs';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, CytoscapejsModule],
-  providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule, CytoscapejsModule],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class AppModule {}
